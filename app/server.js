2c9a74c9272bec879238d70056e04a449b1698af
Tyler Ostinato <tylerostinato@gmail.com>
2022-02-27 12:54:28 -0500

    Updated scramble logic to be more efficient

--- app/server.js
+++ app/server.js
@@ -14,22 +14,72 @@ const pool = new Pool(env);
 
 
 // Source for basic scramble: https://levelup.gitconnected.com/using-javascript-to-scramble-a-rubiks-cube-306f52908f18
+// Parity issue: https://levelup.gitconnected.com/javascript-rubiks-cube-scrambler-part-2-an-improved-algorithm-e279c3731c99
 function makeScramble(){
-    var options = ["F", "R", "U", "B", "L", "D", "F2", "R2", "U2", "B2", "L2", "D2", "F'", "R'", "U'", "B'", "L'", "D'"]
-    var scramble = []
+    let numOptions = [0,1,2,3,4,5]
+    let scramble = []
 
-    // Add 20 moves to the scramble
-    // need to avoid doing parities one after another
-    // example: F then F' basically does nothing
-    for (var i = 0; i < 20; i++) {
-        scramble.push(options[getRandomInt(18)])
+    let bad = true
+    let length = 20; // 20 moves
+
+    while (bad) {
+        scramble = []
+        for (let i = 0; i < length; i++) {
+            scramble.push(numOptions[getRandomInt(6)])
+        }
+        // check if moves directly next to each other are the same letter
+        for (let i = 0; i < length - 1; i++) {
+            if (scramble[i] == scramble[i + 1]) {
+                bad = true
+                break
+            } else {
+                bad = false
+            }
+        }
     }
 
-    return scramble;
+    let options = ["F", "F2", "F'", "R", "R2", "R'", "U", "U2", "U'", "B", "B2", "B'", "L", "L2", "L'", "D", "D2", "D'"];
+    let scrambleMoves = [];
+
+    for (let i = 0; i < 20; i++) {
+        switch (scramble[i]) {
+            case 0:
+                move = options[getRandomInt(3)] // 0,1,2
+                scrambleMoves.push(move)
+                break
+            case 1:
+                move = options[getRandomIntBetween(3, 6)] // 3,4,5
+                scrambleMoves.push(move)
+                break
+            case 2:
+                move = options[getRandomIntBetween(6, 9)] // 6,7,8
+                scrambleMoves.push(move)
+                break
+            case 3:
+                move = options[getRandomIntBetween(9, 12)] // 9,10,11
+                scrambleMoves.push(move)
+                break
+            case 4:
+                move = options[getRandomIntBetween(12, 15)] // 12,13,14
+                scrambleMoves.push(move)
+                break
+            case 5:
+                move = options[getRandomIntBetween(15, 18)] // 15,16,17
+                scrambleMoves.push(move)
+                break
+        }
+    }
+
+    return scrambleMoves;
 }
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+    // returns up to max - 1
+    return Math.floor(Math.random() * Math.floor(max))
+}
+
+function getRandomIntBetween(min, max) { // return a number from min to max - 1. Ex. 3, 9 returns 3 - 8
+    return Math.floor(Math.random() * (max - min) + min)
 }
 
 pool.connect().then(function () {
@@ -65,7 +115,7 @@ app.post("/timer", function(req, res){
 app.get('/scramble', function(req, res){
     let genScramble = [];
     genScramble = makeScramble();
-    console.log(genScramble);
+    // console.log(genScramble);
     // return res.sendStatus(200);
     res.json({scramble: genScramble});
     // res.sendStatus(200);
